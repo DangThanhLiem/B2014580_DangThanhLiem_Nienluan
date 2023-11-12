0f38bcf0137c3820118b16d962109d2f9c716580
B2014580_DangThanhLiem <liemb2014580@student.ctu.edu.vn>
2023-11-12 23:11:22 +0700

    #2 Handle error

--- backend/server.js
+++ backend/server.js
@@ -1,10 +1,13 @@
 const express = require('express')
 require('dotenv').config()
-
+const dbConnect =require('./config/dbconnect')
+const initRouters = require('./routes')
 const app = express()
 const port = process.env.PORT || 8888
 app.use(express.json())
 app.use(express.urlencoded({extended : true}))
+dbConnect()
+initRouters(app)
 
 app.use('/',(req,res)=>{
     res.send("SERVER ON")
